a8dd07f400b6b5f01d710c41aaf9e301bc351780
touraye <touraye07@gmail.com>
2026-08-25 13:09:42 +0000

    fix: remove testimonial carousel section

--- src/app/(public)/about-us/page.tsx
+++ src/app/(public)/about-us/page.tsx
@@ -20,6 +20,6 @@ export default function AboutUsPage() {
         <TeamImageStrip />
         {/* <Clients />         */}
         {/* <TestimonialCarousel /> */}
-        <CareersCTA />
+        {/* <CareersCTA /> */}
     </>)
 }
